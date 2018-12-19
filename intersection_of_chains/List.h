7c5feeffab7638b0d4d4c8ace882a9292b8c24be
KHAKhazeus <khazeus@outlook.com>
2018-12-19 13:53:41 +0800

    Structure of Problem 2

---
+++ intersection_of_chains/List.h
@@ -0,0 +1,142 @@
+//
+// Created by Khazeus on 2018/10/24.
+//
+
+#ifndef DSPROJECT_LIST_H
+#define DSPROJECT_LIST_H
+
+#include "ListNode.h"
+
+//Note: DataType Should be Comparable
+template <typename T>
+class NonDecreaseList{
+
+    typedef T DataType;
+
+public:
+
+    ~NonDecreaseList(){ makeEmpty();}
+    void upSize(){ _size++; }
+    void downSize(){ _size--;}
+    void setSize(int size){ _size = size; }
+    int getSize(){return _size;}
+    NonDecreaseList(): _head(ListNode<DataType>::getNewHeadNode()), _size(0), _rear(nullptr){ _rear = _head; }
+    ListNode<DataType>* gethead(){ return _head; }
+
+    NonDecreaseList intersectionWith(NonDecreaseList& that){
+
+    }
+
+    bool insert(int pos, DataType data){
+        auto temp = gethead();
+        if(pos >= 1 && pos <= getSize() + 1){
+            for(int i = 0; i < pos - 1; i ++){
+                temp = temp->getNext();
+            }
+            auto newNode = ListNode<DataType>::createNode(data);
+
+            if(newNode != nullptr){
+                newNode->setNext(temp->getNext());
+                temp->setNext(newNode);
+                upSize();
+                if(temp == _rear){
+                    _rear = newNode;
+                }
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
+    }
+
+    bool deleteByValue(DataType value){
+        bool result = false;
+        int i = 0;
+        auto temp = gethead();
+        while(temp -> getNext() -> getData() < value){
+            temp = temp->getNext();
+            i++;
+        }
+        if(temp -> getNext() -> getData() == value){
+            result = deleteNodeByPos(++i);
+        }
+        return result;
+    }
+
+    bool searchByValue(DataType value){
+        bool result = false;
+        int i = 0;
+        auto temp = gethead();
+        while(temp -> getNext() -> getData() < value){
+            temp = temp->getNext();
+            i++;
+        }
+        if(temp -> getNext() -> getData() == value){
+            result = true;
+        }
+        return result;
+    }
+
+    bool deleteNodeByPos(int pos){
+        auto temp = gethead();
+        if(pos >= 1 && pos <= getSize()){
+            for(int i = 0; i < pos - 1; i++){
+                temp = temp->getNext();
+            }
+            auto deleteNode = temp->getNext();
+            temp->setNext(deleteNode->getNext());
+            if(deleteNode == _rear){
+                _rear = temp;
+            }
+            downSize();
+            if(deleteNode->getRef() == 0){
+                delete deleteNode;
+            }
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    ListNode<DataType>* searchByPos(int pos){
+        auto goal = gethead();
+        if(pos >= 1 && pos <= getSize()){
+            for(int i = 0; i <= pos - 1; i ++){
+                goal = goal->getNext();
+            }
+            return goal;
+        }
+        else{
+            return nullptr;
+        }
+    }
+
+    void makeEmpty(){
+        while(_head->getNext()){
+            auto cur = _head->getNext();
+            _head->setNext(cur->getNext());
+            cur->deRef();
+            if( cur->getRef() == 0){
+                delete cur;
+            }
+        }
+        _rear = _head;
+        setSize(0);
+    };
+
+
+
+private:
+
+    ListNode<DataType> *_head;
+    int _size;
+    ListNode<DataType> *_rear;
+
+};
+
+#endif //DSPROJECT_LIST_H
